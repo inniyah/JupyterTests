65f461e8a358b685f9c9ada2faf72a9a55c1e4df
Miriam Ruiz <miriam@debian.org>
2022-02-07 01:41:16 +0100

    Added stuff for doing some tmx tests

---
+++ work/tmx-tests/rainbow-tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="rainbow-tiles" tilewidth="32" tileheight="23" tilecount="1230" columns="30">
+ <image source="rainbow-tiles.png" width="960" height="943"/>
+</tileset>
